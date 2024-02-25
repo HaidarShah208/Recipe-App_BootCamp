612aa941458a88259a6e8d8024b0fc7b73be2abf
HaidarShah208 <alhi7896542@gmail.com>
2024-02-25 16:42:26 +0500

    fix search functionality

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Routese } from "./pages/Routese";
+
+const App: React.FC = () => {
+  return <Routese />;
+};
+
+export default App;
---
+++ src/constant/AllAssests.tsx
@@ -0,0 +1,21 @@
+import bg from '../assests/bg.jpg'
+import Toggle from '../assests/toggle.svg'
+import  Browser from '../assests/Browser Icon.svg'
+import Coffe from '../assests/Ico.svg'
+import Search from '../assests/icon.svg'
+import Twitter from '../assests/Twitter Icon.svg'
+import Pinterest from '../assests/Pinterest Icon.svg'
+import Youtube from '../assests/Youtube Icon (1).svg'
+import Left from '../assests/left.svg'
+
+export const IMEGES = {
+MAIN:bg,
+Toggle:Toggle,
+Browser:Browser,
+Coffe:Coffe,
+Pinterest:Pinterest,
+Twitter:Twitter,
+Youtube:Youtube,
+Search:Search,
+Left:Left
+}
